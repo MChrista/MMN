6c058be75736c90255c39a4dff896bd300dbe473
MChrista <mic007.ch@t-online.de>
2015-10-26 19:27:51 +0100

    added support for special characters in task 2

--- assignment02/task2/index.php
+++ assignment02/task2/index.php
@@ -11,10 +11,7 @@
                     
                     for($i = 0; $i < count($word_arr); $i++){
                         $word = $word_arr[$i];
-                        $first_char = substr($word,0,1);
-                        $shuffled_chars = str_shuffle(substr($word,1,strlen($word)-2));
-                        $last_char = substr($word,strlen($word)-1,1);
-                        $word_arr[$i] = $first_char . $shuffled_chars . $last_char;
+                        $word_arr[$i] = shuffleWordWithSpecialCharacters($word);
                     }
                     
                     $shuffled_text = implode(" ",$word_arr);
@@ -29,3 +26,25 @@
 
 
 </html>
+
+
+<?php
+    function shuffleWord($word){
+        $first_char = substr($word,0,1);
+        $shuffled_chars = str_shuffle(substr($word,1,strlen($word)-2));
+        $last_char = substr($word,strlen($word)-1,1);
+        return $first_char . $shuffled_chars . $last_char;
+    }
+
+
+    function shuffleWordWithSpecialCharacters($word){ //Search recursive for special characters
+        if(preg_match('/[.,?!:;]/',$word)){
+            $first_word = preg_replace('/(.*)([.,?!:;])(.*)/','$1',$word);
+            $special_char = preg_replace('/(.*)([.,?!:;])(.*)/','$2',$word);
+            $second_word = preg_replace('/(.*)([.,?!:;])(.*)/','$3',$word);
+            return shuffleWordWithSpecialCharacters($first_word) . $special_char . shuffleWordWithSpecialCharacters($second_word);
+        }else{
+            return shuffleWord($word); //if the word contains no special characters, then shuffle
+        }
+        
+    }
