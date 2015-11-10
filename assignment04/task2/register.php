1db263ff829b849be8923f69ebf4dfabdca5b6bd
MChrista <mic007.ch@t-online.de>
2015-11-10 19:38:41 +0100

    initial commit for assignment 04

---
+++ assignment04/task2/register.php
@@ -0,0 +1,10 @@
+<html>
+    <h2>Registration for Notes</h2>
+    <body>
+        <form id="registerForm" name="registerForm" method="post">
+            
+        
+        
+        </form>
+    </body>
+</html>
