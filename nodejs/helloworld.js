27a90b9859f4fa464333c4b72032855be4565b75
MChrista <mic007.ch@t-online.de>
2015-11-30 19:41:52 +0100

    installed express generator and first try with nodejs

---
+++ nodejs/helloworld.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var port = 8976;
+var host = "127.0.0.1";
+
+var server = http.createServer(function (request, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+});
+
+server.listen(port,host);
