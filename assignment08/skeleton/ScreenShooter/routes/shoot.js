dd2121678f67dc9403ca7e3d48fb233e0583e36c
MChrista <mic007.ch@t-online.de>
2015-12-20 17:43:11 +0100

    results of the lecture on monday and the assignment09. If you want to run this, you need to setup a mongodb with the database mmn-christa and the collection notes

--- assignment08/skeleton/ScreenShooter/routes/shoot.js
+++ assignment08/skeleton/ScreenShooter/routes/shoot.js
@@ -4,18 +4,21 @@ var path = require("path");
 var router = express.Router();
 var shoot = require('./shoot');
 
-router.use(express.static(path.join(__dirname,'../screenshots')));
+router.use('/screenshots',express.static(path.join(__dirname,'../screenshots')));
 
 router.get('/',function (req, res, next){
+    console.log("get request wurde ebenfalls ausgeführt");
     var targetUrl = req.query.url;
     var targetFileName = targetUrl + '.png';
     var targetPath = path.join(__dirname,'../screenshots',targetFileName);
     webshot(targetUrl,targetPath,function(err){
+        console.log('Screenshot saved');
+        var targetWebPath = path.join('/shoot/screenshots',targetFileName);
+        console.log("\n" + targetWebPath + "\n");
+        res.json({"status": "ok","path": targetWebPath, "message":"re-used screenshot"});
     });
     
-    var targetWebPath = path.join('/shoot',targetFileName);
-    console.log("\n" + targetWebPath + "\n");
-    res.json({"status": "ok","path": targetWebPath, "message":"re-used screenshot"});
+    
 });
 
 module.exports = router;
